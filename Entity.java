3e4b4e9b7cfaea2cb62a7cd3d85543dec0db20b1
JIsnor <jgisnor@mta.ca>
2014-09-13 19:10:25 -0300

    Edited

---
+++ Entity.java
@@ -0,0 +1,70 @@
+/*
+ * Idea for avoiding unit-collision: create array of objects
+ * which are ranges of x, y positions (i.e. 4 numbers to make a rectangular
+ * area which is the unit, be it a wall or whatever). The Entity
+ * is passed this array from the controller as illegalAreas ("Wilderness")
+ * and when setCoords is called, it makes sure that the x and y arguments
+ * are not both in any of the illegal ranges.
+ */
+
+/**
+ * The top of the hierarchy, an Entity is a char with an x,y position
+ * and a name. The Representation denotes what the entity is, e.g. Zombie, Human.
+ * Additionally, they have names, e.g. Hunter Bill --> (Representation.HUMAN, x, y, "Bill")
+ * @author James Isnor
+ *
+ */
+public class Entity {
+	
+	//to use with our array of coords
+	private final int X = 0;
+	private final int Y = 1;
+	
+	protected Representation representation;
+	protected int[] coords;
+	protected String name = null;
+	
+	/**
+	 * The only constructor for Entities.
+	 * @param representation - the character associated with what kind of Entity
+	 * this is, e.g. 'Z', 'H', etc.
+	 * @param x - the coordinate
+	 * @param y - the y coordinate
+	 * @param name - the Entity's name
+	 */
+	public Entity(Representation representation, int x, int y, String name) {
+		this.representation = representation;
+		this.name = name;
+		coords = new int[2];
+		coords[X] = x;
+		coords[Y] = y;
+	}
+	
+	/**
+	 * Change the coordinates of the Entity by the specified
+	 * amounts. 
+	 * @param x the value to add to the x coord of this Entity
+	 * @param y the value to add to the y coord of this Entity
+	 */
+	public void changeCoordsBy(int x, int y) {
+		coords[X] += x;
+		coords[Y] += y;
+	}
+	
+	//BELOW THIS LINE LIE GETTERS AND SETTERS (AND RED-SWEATERS)
+	//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	
+	public void setCoords(int x, int y) {
+		coords[X] = y;
+		coords[Y] = y;
+	}
+	
+	public char getRepresentation() {
+		return representation.getCharRep();
+	}
+	
+	public int[] getCoords() {
+		return new int[]{coords[X], coords[Y]};
+	}
+
+}
