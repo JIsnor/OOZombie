0510e9214e11c9197db8cb95eec040d3021e8403
Scrizzard <froggyfanatic@gmail.com>
2014-09-14 10:56:43 -0700

    Update Direction.java
    Added support for random generation and integer conversion

--- Direction.java
+++ Direction.java
@@ -1,10 +1,29 @@
-
 /**
  * Not sure if I could make this any more clear if I tried.
- * @author James Isnor
- *
+ * 
+ * @author James Isnor and Benji
+ * 
  */
 public enum Direction {
-	
 	UP, LEFT, DOWN, RIGHT;
+
+	public static Direction randomDirection() {
+		Direction[] directions = Direction.values();
+		return directions[(int) (Math.random() * directions.length)];
+	}
+	
+	public static Direction intToDirection(int key){
+		switch(key){
+		case 37:
+			return Direction.LEFT;
+		case 38:
+			return Direction.DOWN;
+		case 39:
+			return Direction.RIGHT;
+		case 40:
+			return Direction.UP;
+		default:
+			return null;
+		}
+	}
 }
